d76bc8788e1f1f2836d55b3fc070e552c258a903
marcelolfranca <marcelolfranca@gmail.com>
2018-10-27 17:58:36 -0300

    Instanciando dados no DB H2

--- src/main/java/com/marcelolfranca/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelolfranca/cursomc/CursomcApplication.java
@@ -1,12 +1,38 @@
 package com.marcelolfranca.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marcelolfranca.cursomc.domain.Categoria;
+import com.marcelolfranca.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	//método implementado apenas para inserir algumas categorias ao banco de forma automática
+	//O método run tem que ser implementado quando usa-se o implements CommandLineRunner
+	//É executado sempre que o app é iniciado
+	@Override
+	public void run(String... args) throws Exception {
+
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 }
--- src/main/java/com/marcelolfranca/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcelolfranca/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.marcelolfranca.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +17,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+	//O @PathVariable diz que vai buscar o id que veio da URL
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
