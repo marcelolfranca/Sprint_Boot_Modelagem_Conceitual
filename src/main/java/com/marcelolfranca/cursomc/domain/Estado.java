f714d35fe72b15f0f288e4fffe94a2f396444162
marcelolfranca <marcelolfranca@gmail.com>
2018-10-29 22:04:34 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/marcelolfranca/cursomc/domain/Estado.java
+++ src/main/java/com/marcelolfranca/cursomc/domain/Estado.java
@@ -10,7 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Estado implements Serializable {
@@ -33,7 +33,7 @@ public class Estado implements Serializable {
 	public Estado() {
 	}
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="estado")
 	public List<Cidade> getCidades() {
 		return cidades;
